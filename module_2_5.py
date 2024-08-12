b2b7af407da2902f27c844ad73cf0135ac056970
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-08-12 18:55:22 +0300

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -6,9 +6,10 @@ def get_matrix(n, m, value):
             matrix[i].append(value)
     return matrix
 
-result1 = get_matrix(2,2,10)
-result2 = get_matrix(3,5, 42)
-result3 = get_matrix(4,2,13)
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
 print(result3)
